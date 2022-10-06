033dae901ca21412e21006b8e54d764002b1b4f3
gabriellaxdantas <gabriellaxdantas@gmail.com>
2022-10-06 14:16:43 -0300

    subindo exercícios com alteração

---
+++ codigo/Laboratorio 3/Exercicio 1/Program.cs
@@ -0,0 +1,14 @@
+﻿int comparacoes = 0,  n=10;
+
+    for (int i = 0; i < n; i++){
+        if (n == 10)
+            comparacoes+=1;
+        if ( n == 9)
+            comparacoes+=1;
+        if (n > 10)
+            comparacoes+=1;
+        if (n < 9 )
+            comparacoes+=1;
+        
+    }
+    Console.WriteLine(comparacoes);
